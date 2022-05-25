6d352323db1d11f8fa3985a0e4678b273013b79a
Rui Yang <techoc@foxmail.com>
2022-05-25 14:36:59 +0800

    :heavy_plus_sign:添加路径别名和编辑器路径别名识别

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from "./pages/Login";
-import Layout from "./pages/layout";
+import Login from "@/pages/Login";
+import Layout from "@/pages/layout";
 
 function App() {
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            //约定：使用 @ 表示 src 文件所在路径
+            "@": resolve(__dirname, "src")
+        }
+    }
 })
