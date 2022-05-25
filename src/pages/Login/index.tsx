87ed1b059d0cee5e675b164aa96d458e55eb7f29
Rui Yang <techoc@foxmail.com>
2022-05-25 15:30:13 +0800

    :construction:登录界面

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import {Card} from "antd";
+import {Button, Card, Checkbox, Form, Input} from "antd";
 import logo from "@/assets/logo.png"
 import "./index.scss"
 
@@ -8,6 +8,26 @@ function Login() {
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt=""/>
                 {/* 登录表单 */}
+                <Form initialValues={{remember: true}}>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入手机号"/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入验证码"/>
+                    </Form.Item>
+                    <Form.Item name="remember" valuePropName="checked">
+                        <Checkbox className="login-checkbox-label">
+                            我已阅读并同意「用户协议」和「隐私条款」
+                        </Checkbox>
+                    </Form.Item>
+
+                    <Form.Item>
+                        {/*渲染Button组件为submit按钮*/}
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Card>
         </div>
     )
