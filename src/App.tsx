25fa8d9a0683f1b02c07dd5df4c7f6c9279a0b39
Rui Yang <techoc@foxmail.com>
2022-05-25 14:05:26 +0800

    :heavy_plus_sign:引入react路由和antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./pages/layout";
+
 function App() {
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  )
+    return (
+        //路由配置
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    //创建路由对应关系
+                    <Route path="/" element={<Layout/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {Toaster} from "react-hot-toast";
+//导入antd css
+import 'antd/dist/antd.min.css';
 import "./index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
