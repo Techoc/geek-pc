944ea32f05f58daaa28059467f5058cf8c58151f
techoc <techoc@foxmail.com>
2022-05-29 22:35:28 +0800

    :sparkles:添加登录表单校验

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,36 +1,77 @@
-import {Button, Card, Checkbox, Form, Input} from "antd";
-import logo from "@/assets/logo.png"
-import "./index.scss"
+import { Button, Card, Checkbox, Form, Input } from "antd";
+import logo from "@/assets/logo.png";
+import "./index.scss";
+import React from "react";
 
 function Login() {
-    return (
-        <div className="login">
-            <Card className="login-container">
-                <img className="login-logo" src={logo} alt=""/>
-                {/* 登录表单 */}
-                <Form initialValues={{remember: true}}>
-                    <Form.Item>
-                        <Input size="large" placeholder="请输入手机号"/>
-                    </Form.Item>
-                    <Form.Item>
-                        <Input size="large" placeholder="请输入验证码"/>
-                    </Form.Item>
-                    <Form.Item name="remember" valuePropName="checked">
-                        <Checkbox className="login-checkbox-label">
-                            我已阅读并同意「用户协议」和「隐私条款」
-                        </Checkbox>
-                    </Form.Item>
+  const onFinish = (values: any) => {
+    //values 是表单项里用户输入的数据
+    console.log("Success:", values);
+    //TODO 登录
+  };
 
-                    <Form.Item>
-                        {/*渲染Button组件为submit按钮*/}
-                        <Button type="primary" htmlType="submit" size="large" block>
-                            登录
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </div>
-    )
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form
+          initialValues={{ remember: true }}
+          validateTrigger={["onBlur", "onChange"]}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+                validateTrigger: "onBlur",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "请输入密码!",
+              },
+              {
+                min: 6,
+                message: "密码至少是6位",
+                validateTrigger: "onBlur",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入密码" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            {/*渲染Button组件为submit按钮*/}
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
