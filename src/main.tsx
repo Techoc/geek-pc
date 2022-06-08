b426150347ba2ade3549272982f177f15358cdf7
techoc <techoc@foxmail.com>
2022-06-08 23:51:24 +0800

    :sparkles:富文本编辑器实现

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,13 @@ import zhCN from "antd/lib/locale/zh_CN";
 import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <div>
-    <React.StrictMode>
-      <Toaster />
-      {/*配置语言为中文*/}
-      <ConfigProvider locale={zhCN}>
-        <App />
-      </ConfigProvider>
-    </React.StrictMode>
-  </div>
+    <div>
+        {/*<React.StrictMode>*/}
+        <Toaster />
+        {/*配置语言为中文*/}
+        <ConfigProvider locale={zhCN}>
+            <App />
+        </ConfigProvider>
+        {/*</React.StrictMode>*/}
+    </div>
 );
--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,9 +1,9 @@
 import { Layout, Menu, Popconfirm } from "antd";
 import {
-  HomeOutlined,
-  DiffOutlined,
-  EditOutlined,
-  LogoutOutlined,
+    HomeOutlined,
+    DiffOutlined,
+    EditOutlined,
+    LogoutOutlined,
 } from "@ant-design/icons";
 import "./index.scss";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
@@ -14,64 +14,64 @@ import { observer } from "mobx-react-lite";
 const { Header, Sider } = Layout;
 
 const GeekLayout = () => {
-  let { pathname } = useLocation();
-  let { userStore, loginStore } = useStore();
-  useEffect(() => {
-    userStore.getUserInfo();
-  }, [userStore]);
+    let { pathname } = useLocation();
+    let { userStore, loginStore } = useStore();
+    useEffect(() => {
+        userStore.getUserInfo();
+    }, [userStore]);
 
-  //确定退出
-  let navigate = useNavigate();
-  let onConfirm = () => {
-    //退出登录 删除token 跳回到登录
-    loginStore.logout();
-    navigate("/login");
-  };
+    //确定退出
+    let navigate = useNavigate();
+    let onConfirm = () => {
+        //退出登录 删除token 跳回到登录
+        loginStore.logout();
+        navigate("/login");
+    };
 
-  return (
-    <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <div className="user-info">
-          <span className="user-name">{userStore.userInfo.name}</span>
-          <span className="user-logout">
-            <Popconfirm
-              onConfirm={onConfirm}
-              title="是否确认退出？"
-              okText="退出"
-              cancelText="取消"
-            >
-              <LogoutOutlined /> 退出
-            </Popconfirm>
-          </span>
-        </div>
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            theme="dark"
-            defaultSelectedKeys={[pathname]}
-            style={{ height: "100%", borderRight: 0 }}
-          >
-            <Menu.Item icon={<HomeOutlined />} key="/">
-              <Link to="/">数据概览</Link>
-            </Menu.Item>
-            <Menu.Item icon={<DiffOutlined />} key="/article">
-              <Link to="/article">内容管理</Link>
-            </Menu.Item>
-            <Menu.Item icon={<EditOutlined />} key="/publish">
-              <Link to="/publish">发布文章</Link>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="layout-content" style={{ padding: 20 }}>
-          {/*二级路由出口*/}
-          <Outlet />
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <div className="user-info">
+                    <span className="user-name">{userStore.userInfo.name}</span>
+                    <span className="user-logout">
+                        <Popconfirm
+                            onConfirm={onConfirm}
+                            title="是否确认退出？"
+                            okText="退出"
+                            cancelText="取消"
+                        >
+                            <LogoutOutlined /> 退出
+                        </Popconfirm>
+                    </span>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        theme="dark"
+                        defaultSelectedKeys={[pathname]}
+                        style={{ height: "100%", borderRight: 0 }}
+                    >
+                        <Menu.Item icon={<HomeOutlined />} key="/">
+                            <Link to="/">数据概览</Link>
+                        </Menu.Item>
+                        <Menu.Item icon={<DiffOutlined />} key="/article">
+                            <Link to="/article">内容管理</Link>
+                        </Menu.Item>
+                        <Menu.Item icon={<EditOutlined />} key="/publish">
+                            <Link to="/publish">发布文章</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="layout-content" style={{ padding: 20 }}>
+                    {/*二级路由出口*/}
+                    <Outlet />
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-    </Layout>
-  );
+    );
 };
 
 export default observer(GeekLayout);
